426105458028b06b3bc2effcd820982f224041d2
Manpreet Singh Nehra <manpreet@nehraconsultancy.net>
2021-11-03 23:07:27 +0200

    Add CI pipeline

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# devops-tools
-Devops Tools docker images
+# ci-deploy
+
+Dockerfile and code for an image to with tools used in ci
+1. Awscli
+2. Terraform
+3. Kubectl
+4. helm
+
---
+++ deploy/awscli.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TARGETARCH=$1
+if [[ $TARGETARCH == "amd64" ]]
+then 
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 
+elif [[ $TARGETARCH = "arm64" ]]
+then 
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip"  -o "awscliv2.zip" 
+fi 
+if [[ -f awscliv2.zip ]]
+then
+  unzip -q awscliv2.zip &&  ./aws/install && rm -rf aws
+fi
